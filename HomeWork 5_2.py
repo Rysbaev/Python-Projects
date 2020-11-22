0822cb957cced5cb6e61dbf7003dff4301eee915
Rysbaev Azamat <74239419+Rysbaev@users.noreply.github.com>
2020-11-22 20:45:53 +0600

    Add files via upload

---
+++ HomeWork 5_2.py
@@ -0,0 +1,33 @@
+class Tasks:
+
+    def __init__(self,task, name, date):
+        self.task == task
+        self.name == name
+        self.date == date
+
+    def what_is_your_task(self):
+        return f"{self.task}/{self.name}/{self.date}"
+
+while True:
+
+    print("Hello Dear Guest,This is Your Task Manager! ")
+    tasks = []
+    while True:
+        print("1) Enter Your Task! ")
+        print("2) Show All Your Tasks! ")
+        print("3) End Your Task! ")
+        option = int(input("Choose your option: "))
+
+        if option == 1:
+            task = input("Add Your task: ")
+            name = input("Name of your task: ")
+            date = input("Date of your task: ")
+            tasks.append(task)
+
+        if option == 2:
+            for i in tasks:
+                print(f"{i}")
+
+        if option == 3:
+            idx = int(input("Enter index of your task: "))
+            del tasks[idx]
